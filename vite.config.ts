dd372f64c6d3ba4797ae785723a2c3d5e8f268d8
lmnitzsche <logannitzsche1@gmail.com>
2025-11-08 00:53:40 -0600

    changed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/CornProductionSimulation/',
+  base: '/corn-production-simulation/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
